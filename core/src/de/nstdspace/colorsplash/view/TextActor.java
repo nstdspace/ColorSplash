96814b272ab1d841d8fb6ba7e3cea09206dfcf37
Maksim Rotmann <maksim.rotmann@gmail.com>
2019-07-28 21:05:40 +0200

    fix textactor positioning

--- core/src/de/nstdspace/colorsplash/view/TextActor.java
+++ core/src/de/nstdspace/colorsplash/view/TextActor.java
@@ -1,22 +1,39 @@
 package de.nstdspace.colorsplash.view;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
+/**
+ * using this because to lazy to set up a skin.
+ *
+ * WARNING: none of the Actor methods will work here but
+ * invoking unexpected results.
+ */
 public class TextActor extends Actor {
 
     private BitmapFont font;
     private String text;
 
+    private Texture DEBUG_BG;
+
     public TextActor(String text, BitmapFont font){
         this.text = text;
         this.font = font;
+        GlyphLayout layout = new GlyphLayout(font, text);
+        setSize(layout.width, layout.height);
+
+        DEBUG_BG = ResourceTools.createOneColoredTexture(Color.PINK);
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
-        font.draw(batch, text, 0, 0);
+        Color color = batch.getColor();
+        batch.draw(DEBUG_BG, getX(), getY(), getWidth(), getHeight());
+        font.draw(batch, text, getX(), getY());
     }
 }
