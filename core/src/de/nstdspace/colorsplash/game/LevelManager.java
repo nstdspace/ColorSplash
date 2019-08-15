419bcdbf1f16ac6e32ef82e3ca97a940a28c4838
Maksim Rotmann <maksim.rotmann@gamil.com>
2019-08-15 15:05:26 +0200

    add level manager

--- core/src/de/nstdspace/colorsplash/game/LevelManager.java
+++ core/src/de/nstdspace/colorsplash/game/LevelManager.java
@@ -1,4 +1,55 @@
 package de.nstdspace.colorsplash.game;
 
+import com.badlogic.gdx.graphics.Color;
+
+import java.util.Arrays;
+
+import de.nstdspace.colorsplash.game.gamemode.GameMode;
+import de.nstdspace.colorsplash.game.gamemode.GameModeManager;
+import de.nstdspace.colorsplash.view.GameField;
+
 public class LevelManager {
+
+    public static GameMode initGamemode(int pack, int level){
+        GameMode gameMode = null;
+        if (pack == 1) {
+            if (level == 0) {
+                gameMode = GameModeManager.enrollGameMode1(Arrays.asList(Color.RED, Color.GREEN, Color.BLUE, Color.BROWN), Color.RED, 4);
+                //guiViewContext.addAction(Actions.color(Color.RED, 2f));
+            }
+            if (level == 1) {
+                gameMode = GameModeManager.enrollGameMode1(Arrays.asList(Color.RED, Color.GREEN, Color.BLUE, Color.BROWN), Color.RED, 20);
+            }
+            if (level == 2) {
+                gameMode = GameModeManager.enrollGameModeLike1WithPattern(Arrays.asList(Color.BLACK, Color.YELLOW), Color.BLACK, 10, new GameField.ChangePattern(
+                        new int[][]{
+                                {-1, 0, 1},
+                                {-1, 1, 1},
+                                {1, 0, 1},
+                                {1, -1, 1}
+                        }
+                ));
+            }
+            if (level == 3) {
+                gameMode = GameModeManager.enrollGameModeLike1WithPattern(Arrays.asList(Color.WHITE, Color.PURPLE, Color.VIOLET), Color.VIOLET, 20, new GameField.ChangePattern(
+                        new int[][]{
+                                {0, 1, 1},
+                                {-1, -1, 1},
+                                {1, -1, 1}
+                        }
+                ));
+            }
+            if (level == 4) {
+                gameMode = GameModeManager.enrollGameModeLike1WithPattern(Arrays.asList(Color.YELLOW, Color.ORANGE, Color.CYAN, Color.RED), Color.RED, 20, new GameField.ChangePattern(
+                        new int[][]{
+                                {-1, -1, 2},
+                                {-1, 1, 1},
+                                {1, -1, 1},
+                                {1, 1, 2}
+                        }
+                ));
+            }
+        }
+        return gameMode;
+    }
 }
